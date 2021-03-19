199e81d7cf2646616023dd5864b04f614a22f502
mahshana <mahshanakp@gmail.com>
2021-03-20 01:53:51 +0530

    commit11

---
+++ 11.py
@@ -0,0 +1,6 @@
+square=lambda a:a*a
+print("area of the square is ",square(10))
+rectangle=lambda a,b:a*b
+print("area of rectangle is ",rectangle(4,6))
+triangle=lambda b,h:(b*h)/2
+print("area of triangle ",triangle(5,7))
